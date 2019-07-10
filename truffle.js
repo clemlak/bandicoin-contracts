2ad545396435a331b60b5eae69649e78ec106304
Clement Lakhal <clementlakhal@gmail.com>
2019-07-10 11:04:25 +0200

    Fix Truffle config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PrivateKeyProvider = require('truffle-privatekey-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Utils = require('web3-utils');
 
 const mainnetUrl = `https://mainnet.infura.io/v3/${process.env.INFURA}`;
@@ -15,14 +15,18 @@ module.exports = {
       gasPrice: Utils.toWei('10', 'gwei'),
     },
     ropsten: {
-      provider: new PrivateKeyProvider(process.env.PRIVATE_KEY, ropstenUrl),
+      provider() {
+        return new HDWalletProvider(process.env.MNEMONIC, ropstenUrl, 0);
+      },
       network_id: 3,
-      gasPrice: Utils.toWei('10', 'gwei'),
+      gasPrice: Utils.toWei('20', 'gwei'),
       gas: 4000000,
       skipDryRun: true,
     },
     live: {
-      provider: new PrivateKeyProvider(process.env.PRIVATE_KEY, mainnetUrl),
+      provider() {
+        return new HDWalletProvider(process.env.MNEMONIC, mainnetUrl, 0);
+      },
       network_id: 1,
       gasPrice: Utils.toWei('10', 'gwei'),
       gas: 4000000,
